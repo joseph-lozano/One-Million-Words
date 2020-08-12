2fcffac696f4c1a34dd6962636eace417df83a98
Joseph Lozano <joseph@hey.com>
2020-08-12 02:37:13 -0400

    hardcode url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :one_million_words, OneMillionWordsWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
+  url: [host: "onemillionwords.net", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
